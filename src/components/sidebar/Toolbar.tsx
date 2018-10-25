7c5db6342cae805c2af1239329825a2994af4046
Ryota Mannari <info@manse.jp>
2018-10-26 02:47:41 +0900

    refactor: Make toolbar static

--- src/components/sidebar/Toolbar.tsx
+++ src/components/sidebar/Toolbar.tsx
@@ -1,14 +1,33 @@
-import { ReactNode } from 'react';
+import { inject, observer } from 'mobx-react';
+import { compose } from 'recompose';
+import { PartManagerStore } from '../../stores/PartManagerStore';
+import { IconButton } from '../widget/IconButton';
 import './styles/Toolbar.pcss';
 
 type Props = {
-  leftItems: ReactNode;
-  rightItems: ReactNode;
+  partManagerStore?: PartManagerStore;
 };
 
-export const Toolbar = ({ leftItems, rightItems }: Props) => (
-  <div styleName="base">
-    <div styleName="left">{leftItems}</div>
-    <div styleName="right">{rightItems}</div>
-  </div>
-);
+export const Toolbar = compose<Props, Props>(
+  inject('partManagerStore'),
+  observer,
+)(({ partManagerStore }) => {
+  const noop = () => {};
+  const handleClickAddPart = () => partManagerStore.addPartStore();
+  const handleClickDeletePart = () => partManagerStore.removeActivePartStore();
+  const handleClickRegenerate = () => partManagerStore.refreshActivePartStore();
+  const handleClickSave = () => partManagerStore.saveActivePartStoreAsImage();
+  return (
+    <div styleName="base">
+      <div styleName="left">
+        <IconButton icon="folder" onClick={noop} />
+        <IconButton icon="plus" onClick={handleClickAddPart} />
+        <IconButton icon="trash" disabled={!partManagerStore.activePartStore} onClick={handleClickDeletePart} />
+      </div>
+      <div styleName="right">
+        <IconButton icon="refresh" disabled={!partManagerStore.activePartStore} onClick={handleClickRegenerate} />
+        <IconButton icon="download" disabled={!partManagerStore.activePartStore} onClick={handleClickSave} />
+      </div>
+    </div>
+  );
+});
