c6671fd45a642a624f0a1e9734bd91658a869dcb
Ryota Mannari <info@manse.jp>
2018-11-15 02:44:40 +0900

    feat: Add file loader

--- src/components/sidebar/Toolbar.tsx
+++ src/components/sidebar/Toolbar.tsx
@@ -1,27 +1,40 @@
 import { inject, observer } from 'mobx-react';
 import { compose } from 'recompose';
 import { ApplicationStore, Tab } from '../../stores/ApplicationStore';
+import { ModelStore } from '../../stores/ModelStore';
 import { PartManagerStore } from '../../stores/PartManagerStore';
 import { IconButton } from '../widget/IconButton';
 import './styles/Toolbar.pcss';
 
 type Props = {
   partManagerStore?: PartManagerStore;
+  modelStore?: ModelStore;
   applicationStore?: ApplicationStore;
 };
 
 export const Toolbar = compose<Props, Props>(
   inject('partManagerStore', 'applicationStore'),
   observer,
-)(({ partManagerStore, applicationStore }) => {
-  const noop = () => {};
+)(({ partManagerStore, modelStore, applicationStore }) => {
+  const handleOpen = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.txt, text/plain';
+    input.onchange = event => {
+      const file = (event.target as any).files[0];
+      const reader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = () => modelStore.loadModelFromObjString(reader.result.toString());
+    };
+    input.click();
+  };
   const handleClickAddPart = () => partManagerStore.addPartStore();
   const handleClickDeletePart = () => partManagerStore.removeActivePartStore();
   const handleClickSave = () => partManagerStore.saveActivePartStoreAsImage();
   return (
     <div styleName="base">
       <div styleName="left">
-        <IconButton icon="folder" onClick={noop} />
+        <IconButton icon="folder" onClick={handleOpen} />
         <IconButton icon="plus" onClick={handleClickAddPart} />
         <IconButton icon="trash" disabled={!partManagerStore.activePartStore} onClick={handleClickDeletePart} />
       </div>
